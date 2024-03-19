490e00f04cbf54301298adb432cd8725d4fe4394
liangmingyuan <liangmingyuan@baidu.com>
2024-03-19 12:37:05 +0800

    rgw/reshard: stop recording logs when reshard failed
    A log will be recorded together with index entry in the logrecord
    state, ant it should be stopped when reshard fails. Add a judge
    every certain time at the logrecord state, the reshard has been
    failed if the reshard_lock can be held, then clear the state and
    stop recording logs.
    
    Signed-off-by: Mingyuan Liang <liangmingyuan@baidu.com>

--- src/rgw/rgw_common.cc
+++ src/rgw/rgw_common.cc
@@ -2255,7 +2255,7 @@ void RGWBucketInfo::encode(bufferlist& bl) const {
   const rgw_user* user = std::get_if<rgw_user>(&owner);
   std::string empty;
 
-  ENCODE_START(24, 4, bl);
+  ENCODE_START(25, 4, bl);
   encode(bucket, bl);
   if (user) {
     encode(user->id, bl);
@@ -2303,12 +2303,13 @@ void RGWBucketInfo::encode(bufferlist& bl) const {
   }
   ceph::versioned_variant::encode(owner, bl); // v24
 
+  encode(judge_reshard_lock_time, bl);
   ENCODE_FINISH(bl);
 }
 
 void RGWBucketInfo::decode(bufferlist::const_iterator& bl) {
   rgw_user user;
-  DECODE_START_LEGACY_COMPAT_LEN_32(24, 4, 4, bl);
+  DECODE_START_LEGACY_COMPAT_LEN_32(25, 4, 4, bl);
   decode(bucket, bl);
   if (struct_v >= 2) {
     string s;
@@ -2389,6 +2390,9 @@ void RGWBucketInfo::decode(bufferlist::const_iterator& bl) {
     ceph::versioned_variant::decode(owner, bl);
   } else {
     owner = std::move(user); // user was decoded piecewise above
+  }
+  if (struct_v >= 25) {
+    decode(judge_reshard_lock_time, bl);
   }
 
   if (layout.logs.empty() &&
@@ -2527,6 +2531,8 @@ void RGWBucketInfo::dump(Formatter *f) const
   encode_json("mdsearch_config", mdsearch_config, f);
   encode_json("reshard_status", (int)reshard_status, f);
   encode_json("new_bucket_instance_id", new_bucket_instance_id, f);
+  utime_t jt(judge_reshard_lock_time);
+  encode_json("judge_reshard_lock_time", jt, f);
   if (!empty_sync_policy()) {
     encode_json("sync_policy", *sync_policy, f);
   }
@@ -2568,6 +2574,8 @@ void RGWBucketInfo::decode_json(JSONObj *obj) {
   JSONDecoder::decode_json("reshard_status", rs, obj);
   reshard_status = (cls_rgw_reshard_status)rs;
   JSONDecoder::decode_json("reshard_gen", layout.current_index.layout.normal.reshard_gen, obj);
+  JSONDecoder::decode_json("judge_reshard_lock_time", ut, obj);
+  judge_reshard_lock_time = ut.to_real_time();
   rgw_sync_policy_info sp;
   JSONDecoder::decode_json("sync_policy", sp, obj);
   if (!sp.empty()) {
